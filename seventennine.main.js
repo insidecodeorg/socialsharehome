58062b557cb2fb0d9f3d2db2db015352f22e8348
InsideCode <88227628+insidecodeorg@users.noreply.github.com>
2022-09-06 14:00:18 +0530

    Create seventennine.main.js

---
+++ seventennine.main.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'a6e54c5981a5d90025eda2c0dd329de4',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/a6e54c5981a5d90025eda2c0dd329de4/invoke.js"></scr' + 'ipt>');
